109187a15501c94dfcc4009f647d6796a46ac879
SidneyDuarte <sidney_silva13@outlook.com>
2015-02-23 10:46:31 -0300

    Initial Commit

---
+++ Silver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Silver
+//
+//  Created by Sidney Silva on 23/02/15.
+//  Copyright (c) 2015 Sidney Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Silver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Silver
+//
+//  Created by Sidney Silva on 23/02/15.
+//  Copyright (c) 2015 Sidney Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
